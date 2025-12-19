da9178e5b16fa5fe5e4b15ea9da1b23e956bba92
Shawn Andrews <andrews.shawn@gmail.com>
2025-12-18 18:41:48 -0600

    Update start-lab.sh

--- opt/start-lab.sh
+++ opt/start-lab.sh
@@ -1,36 +1,41 @@
 #!/bin/bash
 
 ##################################
-#                                #
+#
 #      microherd autostart       #
-#                                #
+#
 ##################################
 
 ### Define vars
 
 NAMESPACE=harvester-public
+KUBECTL=/var/lib/rancher/rke2/bin/kubectl
+VIRTCTL=/usr/bin/virtctl
 KUBECONF=/root/microherd.yaml
 
 ### Start control-plane
 
-for vm in $(kubectl get vms -n $NAMESPACE -l microherd=master -o name | cut -d '/' -f2); do
-  virtctl start $vm -n $NAMESPACE
+for vm in $($KUBECTL get vms -n "$NAMESPACE" -l microherd=master -o name | cut -d '/' -f2); do
+  $VIRTCTL start "$vm" -n "$NAMESPACE"
 done
 
 ### Wait for API server to come up
 
-until kubectl --kubeconfig=$KUBECONF cluster-info > /dev/null 2>&1; do
+until $KUBECTL --kubeconfig="$KUBECONF" cluster-info > /dev/null 2>&1; do
   sleep 5
 done
 
 ### Wait for control-plane to be READY
 
-kubectl --kubeconfig=$KUBECONF wait --for=condition=ready node -l node-role.kubernetes.io/control-plane --timeout=600s
+$KUBECTL --kubeconfig="$KUBECONF" wait \
+  --for=condition=ready node \
+  -l node-role.kubernetes.io/control-plane \
+  --timeout=600s
 
 ### Start the workers
 
-for w in $(kubectl get vms -n $NAMESPACE -l microherd=worker -o name | cut -d '/' -f2); do
-  virtctl start $w -n $NAMESPACE
+for w in $($KUBECTL get vms -n "$NAMESPACE" -l microherd=worker -o name | cut -d '/' -f2); do
+  $VIRTCTL start "$w" -n "$NAMESPACE"
 done
 
 ### 1m sleep to allow cluster to come up without sharing the load with Rancher
@@ -39,4 +44,4 @@ sleep 60
 
 ### Start rancher VM
 
-virtctl start rancher -n $NAMESPACE
+$VIRTCTL start rancher -n "$NAMESPACE"
